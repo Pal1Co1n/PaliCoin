3b59dc39eb908c33717c6f02bf97585cc4b7e8c2
Pali Coin <palestinian.co1n@gmail.com>
2025-01-15 09:07:17 +0000

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://palicoin.uk', 
+    generateRobotsTxt: true,       // Generates robots.txt
+    changefreq: 'daily',           // Optional: Change frequency of sitemap
+    priority: 0.7,                 // Optional: Page priority
+    sitemapSize: 5000,             // Optional: Number of entries per sitemap
+  };
+  
